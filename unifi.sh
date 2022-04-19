18c14a5ef7385372621ea1ab4627d9e11819f1c6
Harry Solutions Ltd <102732684+harrysolutions@users.noreply.github.com>
2022-04-19 16:23:47 +0100

    Update unifi.sh

--- unifi.sh
+++ unifi.sh
@@ -1,13 +1,11 @@
 sudo apt update
-sudo apt install --yes apt-transport-https
+sudo apt install --yes apt-transport-https ca-certificates apt-transport-https openjdk-8-jre-headless
 echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
 sudo apt update
-sudo apt install --yes openjdk-8-jre-headless unifi
+sudo apt install --yes unifi
 sudo apt clean
-sudo apt install --yes ca-certificates apt-transport-https
 # sudo ufw enable
 # sudo ufw allow 8443/tcp
 # sudo ufw allow 3478/udp
-sudo service unifi restart
 sudo systemctl status --no-pager --full mongodb.service unifi.service
