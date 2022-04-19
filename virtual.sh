eed5dfe6c07999600cad6fbd8602b8dca3a5470c
Harry Solutions Ltd <102732684+harrysolutions@users.noreply.github.com>
2022-04-19 12:29:55 +0100

    Update virtual.sh

--- virtual.sh
+++ virtual.sh
@@ -1,3 +1,4 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manage
+sudo apt-get install qemu-kvm libvirt-bin bridge-utils virt-manager
