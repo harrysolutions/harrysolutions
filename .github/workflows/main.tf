6dc4456714c1c76e78840e4606bbe4f8165e1a69
Harry Solutions Ltd <102732684+harrysolutions@users.noreply.github.com>
2022-04-20 11:21:02 +0100

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,14 +1,14 @@
-#     # The configuration for the `remote` backend.
-#     terraform {
-#       backend "remote" {
-#         # The name of your Terraform Cloud organization.
-#         organization = "harrysolutions"
-#
-#         # The name of the Terraform Cloud workspace to store Terraform state files in.
-#         workspaces {
-#           name = "harrysolutions"
-#         }
-#       }
-#     }
+     # The configuration for the `remote` backend.
+     terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "harrysolutions"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "harrysolutions"
+        }
+      }
+     }
 - name: HashiCorp - Setup Terraform
   uses: hashicorp/setup-terraform@v1.4.0
