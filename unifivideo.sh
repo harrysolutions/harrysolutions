b064c09b93dd71bf269098663864fb510d071201
Harry Solutions Ltd <102732684+harrysolutions@users.noreply.github.com>
2022-04-20 15:35:55 +0100

    Update unifivideo.sh

--- unifivideo.sh
+++ unifivideo.sh
@@ -1,4 +1,6 @@
-sudo apt-get install mongodb mongodb-server openjdk-8-jre-headless=8u162-b12-1 jsvc
+# File for installing Unifi Video
+
+sudo apt install --yes mongodb mongodb-server openjdk-8-jre-headless=8u162-b12-1 jsvc wget apt-transport-https gnupg
 sudo mv /usr/bin/mongod /usr/bin/mongod.bin
 cd /usr/bin/
 echo "#!/bin/bash
@@ -7,5 +9,4 @@ cleaned_args=$(echo $* | sed -e 's/--nohttpinterface//')
 sudo chmod +x mongod
 wget https://dl.ubnt.com/firmwares/ufv/v3.10.13/unifi-video.Ubuntu18.04_amd64.v3.10.13.deb
 dpkg -i unifi-video.Ubuntu18.04_amd64.v3.10.13.deb
-sudo apt-get install wget apt-transport-https gnupg
 # Host is https://localhost:7443
