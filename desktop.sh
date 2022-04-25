6b8fadce87e0e8c309c0fbd6d66cd0b0f31a2396
Harry Solutions Ltd <102732684+harrysolutions@users.noreply.github.com>
2022-04-25 13:57:58 +0100

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,3 +1,3 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install --yes ubuntu-restricted-extras install chrome-gnome-shell xrdp tasksel
+sudo apt install --yes ubuntu-restricted-extras chrome-gnome-shell xrdp tasksel
