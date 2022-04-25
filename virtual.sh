c7172ddb0ce586fab11270a4f5a6272e3ae7de7d
Harry Solutions Ltd <102732684+harrysolutions@users.noreply.github.com>
2022-04-25 12:28:18 +0100

    Update virtual.sh

--- virtual.sh
+++ virtual.sh
@@ -2,3 +2,6 @@ sudo apt update
 sudo apt upgrade
 sudo apt install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manage
 sudo apt-get install qemu-kvm libvirt-bin bridge-utils virt-manager
+sudo add-apt-repository -y ppa:teejee2008/ppa
+sudo apt-get update
+sudo apt-get install timeshift
