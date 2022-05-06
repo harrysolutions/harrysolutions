441272d6075377ed609b07ee4b9e860abb8176fc
Harry Solutions Ltd <102732684+harrysolutions@users.noreply.github.com>
2022-05-06 16:13:10 +0100

    Create zoneminder.sh

---
+++ zoneminder.sh
@@ -0,0 +1,11 @@
+#This installed the zoneminder software
+sudo apt install mysql-server
+
+sudo add-apt-repository ppa:iconnor/zoneminder-master
+
+sudo apt update
+
+apt install zoneminder
+
+
+mysql -uroot -p < /usr/share/zoneminder/db/zm_create.sql
