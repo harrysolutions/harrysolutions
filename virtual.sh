f8902bf483e4e1e8da174043e5ab08b89a7a819a
Harry Solutions Ltd <102732684+harrysolutions@users.noreply.github.com>
2022-05-24 15:46:29 +0100

    Update virtual.sh

--- virtual.sh
+++ virtual.sh
@@ -1,5 +1,5 @@
 sudo add-apt-repository -y ppa:teejee2008/ppa
 sudo apt update
 sudo apt upgrade
-sudo apt install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager libvirt-bin  timeshift cifs-utils
+sudo apt install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager timeshift cifs-utils
 # nfs-utils
